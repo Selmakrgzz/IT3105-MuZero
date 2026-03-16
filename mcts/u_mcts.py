52a4d87034d531a1c01288dfcede87827b370a32
Selma Karagøz <selma.krgz@outlook.com>
2026-03-16 10:47:25 +0100

    Bugfixes. Make functions compatible with future integration of networks.

--- mcts/u_mcts.py
+++ mcts/u_mcts.py
@@ -1,42 +1,44 @@
 import math
 import random
 
-C = 1.4  # explorasion rate
-
 class MCTSNode:
-    def __init__(self, state, parent=None, action=None):
-        self.state    = state
-        self.parent   = parent
-        self.action   = action
-        self.visits   = 0
-        self.q_value  = 0.0
+    def __init__(self, state, parent=None, action=None, reward=0.0):
+        self.state = state
+        self.parent = parent
+        self.action = action
+        self.reward = reward
+        self.visit_count = 0
+        self.q_value = 0.0
         self.children = []
 
     def is_leaf(self):
         """A leaf node has no childeren yet"""
         return len(self.children) == 0
     
-    def expand(self, gsm):
+    def expand(self, nnp, nnd):
         """
         Add a child per legal action from this node.
-        We'll use gsm to find the legal actions and next state.
         """
-        legal_actions = gsm.get_legal_actions(self.state)
+        node = self
+        policy, _ = nnp.predict(node.state)
 
-        for action in legal_actions:
-            next_state, reward = gsm.get_next_state_and_reward(self.state, action)
+        for action in range(len(policy)):
+            next_abstract_state, reward = nnd.predict(node.state, action)
             child = MCTSNode(
-                state   = next_state,
-                parent  = self,
+                state   = next_abstract_state,
+                parent  = node,
                 action  = action # the action that lead it here
             )
-            self.children.append(child)
+            node.children.append(child)
 
     def ucb1(self, parent):
         """Calculate the UCB1 score for this node"""
-        if self.visits == 0:
+        node = self
+        C = 1.4  # explorasion rate
+        
+        if node.visit_count == 0:
             return float('inf')  # prioritize unvisited nodes
-        return self.q_value + C * math.sqrt(math.log(parent.visits) / self.visits)
+        return node.q_value + C * math.sqrt(math.log(parent.visit_count) / node.visit_count)
 
     def select(self):
         """
@@ -48,70 +50,77 @@ class MCTSNode:
             node = max(node.children, key=lambda child: child.ucb1(node)) # find the child with highest UCB1 score
         return node
 
-    def rollout(self, gsm, depth=5):
+    def rollout(self, depth, nnd, nnp):
         """
         Simulate the game by the number of times in depth from this node and summarize rewards.
         Return total accumulated rewards.
         """
-        state  = self.state
-        total  = 0.0
+        state = self.state
+        accum_reward = []
 
-        for step in range(depth):
-            actions = gsm.get_legal_actions(state)
-            action  = random.choice(actions) # random action for now
-            state, reward = gsm.get_next_state_and_reward(state, action)
-            total += reward
+        for d in range(depth): 
+            policy, _ = nnp.predict(state)
+            action = random.choices(range(len(policy)), weights=policy, k=1)[0]
+            state, reward = nnd.predict(state, action)
+            accum_reward.append(reward)
 
-        # add herustic value of the end state
-        total += gsm.evaluate_state(state)
-        return total
+        # value of the end state
+        _, value = nnp.predict(state)
+        accum_reward.append(value)
+        return accum_reward
     
-    def backprop(self, value):
+    def backprop(self, rewards):
         """
         We'll have to send the value from rollout back to the root.
-        Update visits and q-value for each node on the path.
+        Update visit_count and q-value for each node on the path.
         """
         node = self
-        while node is not None:
-            node.visits  += 1
-            node.q_value += (value - node.q_value) / node.visits
-            node = node.parent
-    
+        node.visit_count += 1
+        node.q_value += (sum(rewards) - node.q_value) / node.visit_count
+
+        if node.parent is not None:
+            rewards.append(node.reward) 
+            node.parent.backprop(rewards)
+        
 class MCTS:
-    def __init__(self, gsm, num_searches=50, rollout_depth=5):
-        self.gsm = gsm
+    def __init__(self, num_searches=50, rollout_depth=5):
         self.num_searches = num_searches
         self.rollout_depth = rollout_depth
 
-    def search(self, root_state):
+    def search(self, root_states, nnr, nnd, nnp):
         """
         Run MCTS from a given state and return
         the best action + policy + value
         """
-        root = MCTSNode(root_state)
-        root.expand(self.gsm)
+        node = self
+        abstract_state = nnr.represent(root_states)
+        root = MCTSNode(abstract_state)
+        root.expand(nnd, nnp)
 
-        for step in range(self.num_searches):
+        for step in range(node.num_searches):
             # 1. SELECT - visit a leaf
             leaf = root.select()
 
             # 2. EXPAND - expand child if it hasen't been visited
-            if leaf.visits > 0:
-                leaf.expand(self.gsm)
-                leaf = random.choice(leaf.children)
+            if leaf.visit_count > 0:
+                leaf.expand(nnd, nnp)
+                if leaf.children:
+                    leaf = random.choice(leaf.children)
 
             # 3. ROLLOUT - estimate value
-            value = leaf.rollout(self.gsm, self.rollout_depth)
+            accum_reward = leaf.rollout(node.rollout_depth, nnd, nnp)
 
             # 4. BACKPROP - send value back up
-            leaf.backprop(value)
+            leaf.backprop(accum_reward)
 
         # pick best action based on number of visitis after all searches
-        best_child = max(root.children, key=lambda c: c.visits)
+        best_child = max(root.children, key=lambda c: c.visit_count)
         
         # policy
-        total_visits = sum(c.visits for c in root.children)
-        policy = {c.action: c.visits / total_visits for c in root.children} # how often did mcts choose each action
+        total_visits = sum(child.visit_count for child in root.children)
+        policy = [0.0,0.0,0.0]
+        for child in root.children:
+            policy[child.action] = child.visit_count / total_visits # how often did mcts choose each action
         
         # value of the root node
         value = root.q_value
